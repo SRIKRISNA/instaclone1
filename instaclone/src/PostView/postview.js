8d4a17b63b57a3ac40e817430e34cca4f7053723
Srikrishna <krishna333sp@gmail.com>
2022-07-23 15:35:34 +0530

    design view

--- instaclone/src/PostView/postview.js
+++ instaclone/src/PostView/postview.js
@@ -56,6 +56,9 @@ const PostView = ()=> {
                         })
                     }
                 </div>
+                <div id="footer">
+                    <p style={{"textAlign":"center"}}>Designed and Developed By <a href="https://www.allwebsites.in/" style={{"color":"antiquewhite"}}>SRIKRISHNA</a> &#169; All Rights Reserved.</p>
+                </div>
             </div>
         </>
     )
